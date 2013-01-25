98b0112ced0f4dc835ec920942127f83f1b114a1
Tadayoshi Sato <tadayosi@gmail.com>
2013-01-26 00:02:40 +0900

    "basic-contract-first" sample

---
+++ basic-contract-first/src/main/java/com/redhat/samples/ws/basic_contract_first/GreetingServiceImpl.java
@@ -0,0 +1,37 @@
+package com.redhat.samples.ws.basic_contract_first;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+import org.apache.log4j.Logger;
+
+@WebService(serviceName = "GreetingService", portName = "GreetingServicePort", wsdlLocation = "/WEB-INF/wsdl/greeting.wsdl")
+public class GreetingServiceImpl implements GreetingService {
+  private static final Logger LOGGER = Logger.getLogger(GreetingServiceImpl.class);
+
+  @WebMethod
+  public String hello(@WebParam(name = "name") String name) {
+    String message = String.format("Hello, %s!", name);
+    LOGGER.info(message);
+    return message;
+  }
+
+  @WebMethod
+  public String goodbye(@WebParam(name = "name") String name) {
+    String message = String.format("Goodbye, %s!", name);
+    LOGGER.info(message);
+    return message;
+  }
+
+  public static void main(String[] args) {
+    try {
+      Endpoint.publish("http://localhost:18080/greeting", new GreetingServiceImpl());
+    } catch (Throwable t) {
+      t.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+}
