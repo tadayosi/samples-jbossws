32f0987d080583b9443a962d3307e1d0a67601dd
Tadayoshi Sato <tadayosi@gmail.com>
2014-01-21 11:27:39 +0900

    basic-contract-first: reformatted

--- basic-contract-first/src/main/java/com/redhat/samples/ws/GreetingServiceImpl.java
+++ basic-contract-first/src/main/java/com/redhat/samples/ws/GreetingServiceImpl.java
@@ -8,33 +8,33 @@ import javax.xml.ws.Endpoint;
 import org.apache.log4j.Logger;
 
 @WebService(
-    serviceName = "GreetingService",
-    portName = "GreetingServicePort",
-    wsdlLocation = "/META-INF/wsdl/greeting.wsdl")
+        serviceName = "GreetingService",
+        portName = "GreetingServicePort",
+        wsdlLocation = "/META-INF/wsdl/greeting.wsdl")
 public class GreetingServiceImpl implements GreetingService {
-  private static final Logger LOGGER = Logger.getLogger(GreetingServiceImpl.class);
+    private static final Logger LOGGER = Logger.getLogger(GreetingServiceImpl.class);
 
-  @WebMethod
-  public String hello(@WebParam(name = "name") String name) {
-    String message = String.format("Hello, %s!", name);
-    LOGGER.info(message);
-    return message;
-  }
-
-  @WebMethod
-  public String goodbye(@WebParam(name = "name") String name) {
-    String message = String.format("Goodbye, %s!", name);
-    LOGGER.info(message);
-    return message;
-  }
-
-  public static void main(String[] args) {
-    try {
-      Endpoint.publish("http://localhost:18080/greeting", new GreetingServiceImpl());
-    } catch (Throwable t) {
-      t.printStackTrace();
-      System.exit(1);
+    @WebMethod
+    public String hello(@WebParam(name = "name") String name) {
+        String message = String.format("Hello, %s!", name);
+        LOGGER.info(message);
+        return message;
     }
-  }
+
+    @WebMethod
+    public String goodbye(@WebParam(name = "name") String name) {
+        String message = String.format("Goodbye, %s!", name);
+        LOGGER.info(message);
+        return message;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Endpoint.publish("http://localhost:18080/greeting", new GreetingServiceImpl());
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
 
 }
