492a41752bcc045c88f1946e1d162d121682a764
Tadayoshi Sato <tadayosi@gmail.com>
2014-01-21 11:21:19 +0900

    schema-validation-cxf: made the sample EAP 6 compliant

--- schema-validation-cxf/src/main/java/com/redhat/samples/ws/GreetingServiceImpl.java
+++ schema-validation-cxf/src/main/java/com/redhat/samples/ws/GreetingServiceImpl.java
@@ -8,10 +8,7 @@ import javax.xml.ws.Endpoint;
 import org.apache.cxf.annotations.SchemaValidation;
 import org.apache.log4j.Logger;
 
-@WebService(
-        serviceName = "GreetingService",
-        portName = "GreetingServicePort",
-        wsdlLocation = "classpath:/META-INF/greeting.wsdl")
+@WebService(serviceName = "GreetingService", portName = "GreetingServicePort", wsdlLocation = "/META-INF/greeting.wsdl")
 @SchemaValidation
 public class GreetingServiceImpl implements GreetingService {
     private static final Logger LOGGER = Logger.getLogger(GreetingServiceImpl.class);
