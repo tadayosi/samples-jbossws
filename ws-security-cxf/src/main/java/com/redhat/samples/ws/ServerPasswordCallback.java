4924a7304b45de73e4d677b032c88110d5682342
Tadayoshi Sato <tadayosi@gmail.com>
2014-03-17 15:58:33 +0900

    ws-security-cxf: reformatted

--- ws-security-cxf/src/main/java/com/redhat/samples/ws/ServerPasswordCallback.java
+++ ws-security-cxf/src/main/java/com/redhat/samples/ws/ServerPasswordCallback.java
@@ -10,16 +10,17 @@ import org.apache.log4j.Logger;
 import org.apache.ws.security.WSPasswordCallback;
 
 public class ServerPasswordCallback implements CallbackHandler {
-  private static final Logger LOGGER   = Logger.getLogger(ServerPasswordCallback.class);
+    private static final Logger LOGGER = Logger.getLogger(ServerPasswordCallback.class);
 
-  private static final String USERNAME = "kermit";
-  private static final String PASSWORD = "thefrog";
+    private static final String USERNAME = "kermit";
+    private static final String PASSWORD = "thefrog";
 
-  public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-    WSPasswordCallback callback = (WSPasswordCallback) callbacks[0];
-    LOGGER.info(String.format("%s = %s", callback.getIdentifier(), callback.getPassword()));
-    if (USERNAME.equals(callback.getIdentifier()) && PASSWORD.equals(callback.getPassword())) { return; }
-    throw new SecurityException(String.format("User '%s' with password '%s' not allowed.", callback.getIdentifier(), callback.getPassword()));
-  }
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        WSPasswordCallback callback = (WSPasswordCallback) callbacks[0];
+        LOGGER.info(String.format("%s = %s", callback.getIdentifier(), callback.getPassword()));
+        if (USERNAME.equals(callback.getIdentifier()) && PASSWORD.equals(callback.getPassword())) { return; }
+        throw new SecurityException(String.format("User '%s' with password '%s' not allowed.",
+                callback.getIdentifier(), callback.getPassword()));
+    }
 
 }
