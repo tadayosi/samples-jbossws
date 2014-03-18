255c215c3f31a0263a89e18a65ce21eb9a242645
Tadayoshi Sato <tadayosi@gmail.com>
2014-03-18 16:37:21 +0900

    "ws-rm" sample -- demonstrates how to use WS-ReliableMessaging in JBoss WS

---
+++ ws-rm/client/src/main/java/com/redhat/samples/ws/client/GreetingClient.java
@@ -0,0 +1,45 @@
+package com.redhat.samples.ws.client;
+
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+
+import org.apache.log4j.Logger;
+
+import com.redhat.samples.ws.GreetingService;
+
+public class GreetingClient {
+
+    private static final Logger LOGGER = Logger.getLogger(GreetingClient.class);
+
+    private static final URL WSDL = GreetingClient.class.getResource("/META-INF/greeting.wsdl");
+    private static final QName SERVICE = new QName("http://ws.samples.redhat.com/", "GreetingService");
+    //private static final String ENDPOINT = "http://localhost:18080/greeting";
+    private static final String ENDPOINT = "http://localhost:8080/sample/greeting";
+
+    public static void main(String[] args) {
+        new GreetingClient().invokeService(GreetingClient.class.getSimpleName(), new PrintWriter(System.out, true));
+    }
+
+    public void invokeService(String name, PrintWriter writer) {
+        GreetingService service = createService();
+        String helloResponse = service.hello(name);
+        String goodbyeResponse = service.goodbye(name);
+        LOGGER.info(helloResponse);
+        LOGGER.info(goodbyeResponse);
+        writer.println(helloResponse);
+        writer.println(goodbyeResponse);
+    }
+
+    private static GreetingService createService() {
+        GreetingService service = Service.create(WSDL, SERVICE).getPort(GreetingService.class);
+        Map<String, Object> context = ((BindingProvider) service).getRequestContext();
+        context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, ENDPOINT);
+        return service;
+    }
+
+}
