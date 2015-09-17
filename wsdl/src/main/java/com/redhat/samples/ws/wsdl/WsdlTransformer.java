44cc3a182fdeb793f1a99f1e6ef2ba94511092d7
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2015-09-17 14:40:04 +0900

    wsdl: fixed tiny warning/error & formatted

--- wsdl/src/main/java/com/redhat/samples/ws/wsdl/WsdlTransformer.java
+++ wsdl/src/main/java/com/redhat/samples/ws/wsdl/WsdlTransformer.java
@@ -17,62 +17,62 @@ import javax.wsdl.xml.WSDLWriter;
 
 public class WsdlTransformer {
 
-  private WSDLFactory factory;
+    private WSDLFactory factory;
 
-  public WsdlTransformer() {
-    try {
-      factory = WSDLFactory.newInstance();
-    } catch (WSDLException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
-    }
-  }
-
-  private WSDLReader wsdlReader() {
-    WSDLReader reader = factory.newWSDLReader();
-    reader.setFeature("javax.wsdl.verbose", true); // true by default
-    reader.setFeature("javax.wsdl.importDocuments", true); //  true by default
-    return reader;
-  }
-
-  private WSDLWriter wsdlWriter() {
-    WSDLWriter writer = factory.newWSDLWriter();
-    return writer;
-  }
-
-  public String inlineSchemas(String wsdl) {
-    try {
-      Definition definition = wsdlReader().readWSDL(wsdl);
-      doInlineSchemas(definition.getTypes());
-      StringWriter inlined = new StringWriter();
-      wsdlWriter().writeWSDL(definition, inlined);
-      return inlined.toString();
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
-    }
-  }
-
-  private void doInlineSchemas(Types types) {
-    List<Schema> externalSchemas = new ArrayList<Schema>();
-    for (Object element : types.getExtensibilityElements()) {
-      Schema schema = (Schema) element;
-      // imports
-      for (Object key : schema.getImports().keySet()) {
-        for (Object value : (Vector<?>) schema.getImports().get(key)) {
-          SchemaImport schemaImport = (SchemaImport) value;
-          externalSchemas.add(schemaImport.getReferencedSchema());
+    public WsdlTransformer() {
+        try {
+            factory = WSDLFactory.newInstance();
+        } catch (WSDLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-      }
-      // includes
-      for (Object include : schema.getIncludes()) {
-        externalSchemas.add(((SchemaReference) include).getReferencedSchema());
-      }
     }
-    types.getExtensibilityElements().clear();
-    for (Schema schema : externalSchemas) {
-      types.addExtensibilityElement(schema);
+
+    private WSDLReader wsdlReader() {
+        WSDLReader reader = factory.newWSDLReader();
+        reader.setFeature("javax.wsdl.verbose", true); // true by default
+        reader.setFeature("javax.wsdl.importDocuments", true); //  true by default
+        return reader;
     }
-  }
+
+    private WSDLWriter wsdlWriter() {
+        WSDLWriter writer = factory.newWSDLWriter();
+        return writer;
+    }
+
+    public String inlineSchemas(String wsdl) {
+        try {
+            Definition definition = wsdlReader().readWSDL(wsdl);
+            doInlineSchemas(definition.getTypes());
+            StringWriter inlined = new StringWriter();
+            wsdlWriter().writeWSDL(definition, inlined);
+            return inlined.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doInlineSchemas(Types types) {
+        List<Schema> externalSchemas = new ArrayList<Schema>();
+        for (Object element : types.getExtensibilityElements()) {
+            Schema schema = (Schema) element;
+            // imports
+            for (Object key : schema.getImports().keySet()) {
+                for (Object value : (Vector<?>) schema.getImports().get(key)) {
+                    SchemaImport schemaImport = (SchemaImport) value;
+                    externalSchemas.add(schemaImport.getReferencedSchema());
+                }
+            }
+            // includes
+            for (Object include : schema.getIncludes()) {
+                externalSchemas.add(((SchemaReference) include).getReferencedSchema());
+            }
+        }
+        types.getExtensibilityElements().clear();
+        for (Schema schema : externalSchemas) {
+            types.addExtensibilityElement(schema);
+        }
+    }
 
 }
--- wsdl/src/test/java/com/redhat/samples/ws/wsdl/WsdlTransformerTest.java
+++ wsdl/src/test/java/com/redhat/samples/ws/wsdl/WsdlTransformerTest.java
@@ -11,24 +11,24 @@ import org.junit.Test;
 
 public class WsdlTransformerTest {
 
-  @Before
-  public void setUp() {
-    XMLUnit.setIgnoreWhitespace(true);
-  }
+    @Before
+    public void setUp() {
+        XMLUnit.setIgnoreWhitespace(true);
+    }
 
-  @Test
-  public void inlineSchemas() throws Exception {
-    String expected = IOUtils.toString(getClass().getResource("/wsdl/expected.wsdl"));
-    String wsdl = getClass().getResource("/wsdl/greeting.wsdl").getPath();
-    String inlined = new WsdlTransformer().inlineSchemas(wsdl);
-    System.out.println(inlined);
-    assertWSDLEqual(expected, inlined);
-  }
+    @Test
+    public void inlineSchemas() throws Exception {
+        String expected = IOUtils.toString(getClass().getResource("/wsdl/expected.wsdl"));
+        String wsdl = getClass().getResource("/wsdl/greeting.wsdl").getPath();
+        String inlined = new WsdlTransformer().inlineSchemas(wsdl);
+        System.out.println(inlined);
+        assertWSDLEqual(expected, inlined);
+    }
 
-  private static void assertWSDLEqual(String expected, String inlined) throws Exception {
-    Diff diff = new Diff(expected, inlined);
-    diff.overrideElementQualifier(new ElementNameAndAttributeQualifier());
-    assertXMLEqual(diff, true);
-  }
+    private static void assertWSDLEqual(String expected, String inlined) throws Exception {
+        Diff diff = new Diff(expected, inlined);
+        diff.overrideElementQualifier(new ElementNameAndAttributeQualifier());
+        assertXMLEqual(diff, true);
+    }
 
 }
